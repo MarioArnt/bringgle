25b79878750810c57971bb0cf786b05b83196bd8
Mario Arnautou <mario@arnautou.fr>
2018-07-13 20:15:02 +0200

    Set global config for axios

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-export default() => {
-  return axios.create({
-    baseURL: `http://localhost:8081`
-  })
+const defaultConfig = {
+  baseURL: 'http://localhost:8081/api'
 }
+
+export default axios.create(defaultConfig)
--- src/components/CreateList.vue
+++ src/components/CreateList.vue
@@ -1,45 +1,57 @@
 <template lang='pug'>
-.row
-  h1 Create new list
-  form.col.s12
-    div.row
-      div.input-field.col.s12
-        input(v-validate="'required'" v-model="listName" placeholder='Awesome List' id='list-name' name="list-name" type='text' class='validate' required)
-        label(for='list-name') List Name
-        span.helper-text(:data-error="errors.first('list-name')")
-        span {{ errors.first('list-name') }}
-    div.row
-      div.input-field.col.s12
-        input(v-validate="'required|email'" v-model="email" placeholder='john.doe@mail.com' id='user-email' name="user-email" type='email' class='validate' required)
-        label(for='user-email') Email
-        span.helper-text(:data-error="errors.first('user-email')")
-        span {{ errors.first('user-email') }}
-    div.row
-      div.input-field.col.s12
-        input(v-validate="'required'" v-model="displayName" placeholder='John Doe' id='user-name' name="user-name" type='text' class='validate' required)
-        label(for='user-name') Display Name
-        span.helper-text(:data-error="errors.first('user-name')")
-        span {{ errors.first('user-name') }}
-    div.row
-      div.input-field.col.s12
-        button(:disabled="errors.any() || !this.displayName || !this.listName || !this.userEmail " v-on:click='sendData()').waves-effect.waves-light.btn
-          i.fa.fa-plus
-          | Create
+  .row
+    h1 Create new list
+    form.col.s12
+      div.row
+        div.input-field.col.s12
+          input(v-validate="'required'" v-model="listName" placeholder='Awesome List' id='list-name' name="list-name" type='text' class='validate' required)
+          label(for='list-name') List Name
+          span.helper-text(:data-error="errors.first('list-name')")
+          span {{ errors.first('list-name') }}
+      div.row
+        div.input-field.col.s12
+          input(v-validate="'required|email'" v-model="email" placeholder='john.doe@mail.com' id='user-email' name="user-email" type='email' class='validate' required)
+          label(for='user-email') Email
+          span.helper-text(:data-error="errors.first('user-email')")
+          span {{ errors.first('user-email') }}
+      div.row
+        div.input-field.col.s12
+          input(v-validate="'required'" v-model="displayName" placeholder='John Doe' id='user-name' name="user-name" type='text' class='validate' required)
+          label(for='user-name') Display Name
+          span.helper-text(:data-error="errors.first('user-name')")
+          span {{ errors.first('user-name') }}
+      div.row
+        div.input-field.col.s12
+          button(:disabled="errors.any()" v-on:click='sendData()').waves-effect.waves-light.btn
+            i.fa.fa-plus
+            | Create
 </template>
 
 <script>
-import Api from '@/api'
+import axiosClient from '../api/index.js'
 
 export default {
   name: 'Test',
+  data: function () {
+    return {
+      displayName: '',
+      email: '',
+      listName: ''
+    }
+  },
   methods: {
     sendData () {
+      axiosClient.get('posts')
       if (!this.errors.any() && !!this.displayName && !!this.listName && !!this.userEmail) {
-        return Api().post('lists', {
-          displayName: this.displayName,
-          listName: this.listName,
-          userEmail: this.userEmail
-        })
+        /* http.request({
+          url: 'https://jsonplaceholder.typicode.com',
+          method: 'post',
+          data: {
+            displayName: this.displayName,
+            listName: this.listName,
+            userEmail: this.userEmail
+          }
+        }) */
       }
     }
   }
