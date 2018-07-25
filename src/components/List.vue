e7af71a085a474956c5200ab9361837fd94fd9e6
Mario Arnautou <mario@arnautou.fr>
2018-07-25 11:44:06 +0200

    Set up lists state management and reactivity with vuex

--- src/components/List.vue
+++ src/components/List.vue
@@ -2,10 +2,10 @@
   .list
     h1(v-if="error") Error occured
     .list-content(v-if="loaded")
-      h1 {{  list.title }}
+      h1 {{  $store.state.currentList.title }}
       h3 Attendees
       div#attendees
-        div.attendee(v-for="attendee in list.attendees")
+        div.attendee(v-for="attendee in $store.state.currentList.attendees")
           md-card(md-with-hover)
             md-ripple
               md-card-header
@@ -13,7 +13,7 @@
                 div.md-subhead {{ attendee.connected ? 'Online' : 'Offline' }}
       h3 Items
         ul
-          li(v-for="item in list.items") {{ item.name }}
+          li(v-for="item in $store.state.currentList.items") {{ item.name }}
 </template>
 
 <script>
@@ -22,23 +22,16 @@ import router from '@/router'
 import Logger from 'js-logger'
 import io from 'socket.io-client'
 
-function userConnectedOrDisconnected (connectedUsers, attendees) {
-  Logger.info('Connected: ', connectedUsers)
-  attendees.forEach(att => {
-    att.connected = false
-  })
-  connectedUsers.forEach(usr => {
-    attendees.find(att => att.id === usr).connected = true
-  })
-  Logger.debug(attendees)
+function userConnectedOrDisconnected (store, connected) {
+  Logger.info('Connected: ', connected)
+  store.commit('changeConnectedUsers', connected)
 }
 
 export default {
   data: function () {
     return {
       loaded: false,
-      error: false,
-      list: {}
+      error: false
     }
   },
   name: 'List',
@@ -47,17 +40,17 @@ export default {
       Logger.debug('Loading list with ID', this.$route.params.id)
       if (res.data.attendees.some((att) => att.id === this.$store.state.currentUser.id)) {
         Logger.debug('Current user is an attendee', this.$store.state.currentUser)
-        this.list = res.data
+        this.$store.commit('changeCurrentList', res.data)
         this.loaded = true
-        Logger.debug('List loaded', this.list)
+        Logger.debug('List loaded', this.$store.state.currentList)
         const socket = io('localhost:8081', {
           query: {
             userId: this.$store.state.currentUser.id,
-            listId: this.list.id
+            listId: this.$store.state.currentList.id
           }
         })
-        socket.on('user connected', connected => userConnectedOrDisconnected(connected, this.list.attendees))
-        socket.on('user disconnected', connected => userConnectedOrDisconnected(connected, this.list.attendees))
+        socket.on('user connected', connected => userConnectedOrDisconnected(this.$store, connected))
+        socket.on('user disconnected', connected => userConnectedOrDisconnected(this.$store, connected))
         Logger.info('Socket created', socket)
       } else {
         Logger.info('Current user is not an attendee, redirecting...', this.$store.state.currentUser)
--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,62 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Logger from 'js-logger'
 
 Vue.use(Vuex)
 
+const setProperty = (oldState, newState, prop) => {
+  if (newState[prop]) Vue.set(oldState, prop, newState[prop])
+  else Vue.delete(oldState, prop)
+}
+
 const store = new Vuex.Store({
   state: {
-    currentUser: {}
+    currentUser: {},
+    currentList: {
+      id: '',
+      title: '',
+      owner: {},
+      attendees: [],
+      connected: [],
+      items: []
+    }
   },
   mutations: {
     changeCurrentUser (state, user) {
-      if (user.id) Vue.set(state.currentUser, 'id', user.id)
-      else Vue.delete(state.currentUser, 'id')
-      if (user.name) Vue.set(state.currentUser, 'name', user.name)
-      else Vue.delete(state.currentUser, 'name')
-      if (user.email) Vue.set(state.currentUser, 'email', user.email)
-      else Vue.delete(state.currentUser, 'email')
+      Logger.debug('Changing current user')
+      Logger.debug('Old value: ', state.currentUser)
+      Logger.debug('New value', user)
+      setProperty(state.currentUser, user, 'id')
+      setProperty(state.currentUser, user, 'name')
+      setProperty(state.currentUser, user, 'email')
+    },
+    changeCurrentList (state, list) {
+      Logger.debug('Changing current list')
+      Logger.debug('Old value: ', state.currentList)
+      Logger.debug('New value', list)
+      setProperty(state.currentList, list, 'id')
+      setProperty(state.currentList, list, 'title')
+      setProperty(state.currentList, list, 'owner')
+      setProperty(state.currentList, list, 'attendees')
+      setProperty(state.currentList, list, 'items')
+    },
+    changeConnectedUsers (state, connected) {
+      state.currentList.attendees.forEach((att) => Vue.set(att, 'connected', false))
+      if (connected) {
+        Vue.set(state.currentList, 'connected', connected)
+        connected.forEach(uid => {
+          const attendee = state.currentList.attendees.find(att => att.id === uid)
+          if (attendee) {
+            Vue.set(attendee, 'connected', true)
+          }
+        })
+      } else {
+        Vue.delete(state.currentList, 'connected')
+      }
+      Logger.debug('New value', state.currentList)
+    },
+    addAttendee (state, attendee) {
+      state.currentList.attendees.push(attendee)
     }
   }
 })
